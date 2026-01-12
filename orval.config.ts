c28587491426e1d5ff043ee81cc1374209381483
dg50914 <dg50914@student.uni-lj.si>
2026-01-12 22:59:26 +0100

    Generate the rest

--- orval.config.ts
+++ orval.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'orval'
 
 export default defineConfig({
+  auth: {
+    input: {
+      target: '../auth/api/docs/swagger.yaml',
+    },
+    output: {
+      mode: 'tags-split',
+      target: 'src/api/auth',
+      schemas: 'src/api/auth/model',
+      client: 'vue-query',
+      mock: true,
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+
   nakup: {
+    input: {
+      target: '../nakup/api/docs/swagger.yaml',
+    },
     output: {
       mode: 'tags-split',
       target: 'src/api/nakup',
@@ -9,8 +28,37 @@ export default defineConfig({
       client: 'vue-query',
       mock: true,
     },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+
+  spored: {
     input: {
-      target: '../nakup/api/docs/swagger.yaml',
+      target: '../spored/api/docs/swagger.yaml',
+    },
+    output: {
+      mode: 'tags-split',
+      target: 'src/api/spored',
+      schemas: 'src/api/spored/model',
+      client: 'vue-query',
+      mock: true,
+    },
+    hooks: {
+      afterAllFilesWrite: 'prettier --write',
+    },
+  },
+
+  reklame: {
+    input: {
+      target: '../reklame/api/docs/swagger.yaml',
+    },
+    output: {
+      mode: 'tags-split',
+      target: 'src/api/reklame',
+      schemas: 'src/api/reklame/model',
+      client: 'vue-query',
+      mock: true,
     },
     hooks: {
       afterAllFilesWrite: 'prettier --write',
