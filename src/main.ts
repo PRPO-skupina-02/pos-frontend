1c65b335874d3a5725e8fccc85149ae17231f8f5
dg50914 <dg50914@student.uni-lj.si>
2026-01-12 23:13:37 +0100

    Add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/main.css'
 
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(VueQueryPlugin)
 app.use(router)
 
 app.mount('#app')
